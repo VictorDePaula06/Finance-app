6eb102793cfa0ecc45c6ca7bbbfd4f96e3493da2
Joao victor <jvictor@gmail.com>
2025-12-07 19:41:58 -0300

    Segurança: Movendo chaves para variaveis de ambiente

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtOnQ63qNpiA92onErwTkdR7qgCMgt6I8",
-    authDomain: "finance-app-fe605.firebaseapp.com",
-    projectId: "finance-app-fe605",
-    storageBucket: "finance-app-fe605.firebasestorage.app",
-    messagingSenderId: "69179787748",
-    appId: "1:69179787748:web:8a03854ff1c4dc00115ff1",
-    measurementId: "G-SFCY2B4NK6"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
